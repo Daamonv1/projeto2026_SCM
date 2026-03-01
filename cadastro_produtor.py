1f3917ce64787ecda69ddb336769f721297cbd7b
Daamonv1 <arthurzika077@gmail.com>
2026-03-01 18:41:57 -0300

    adiciona modulo de cadastro

--- cadastro_produtor.py
+++ cadastro_produtor.py
@@ -1,1 +1,18 @@
-print('módulo de cadastro')
+#Modulo de cadastro de produtor rural - AgroApp 2026
+
+def cadastrar_produtor (nome, cpf, municipio, area_ha):
+
+#Cadastra um novo produtor rural no sistema.
+
+    produtor =  {
+
+"nome": nome,
+"cpf": cpf,
+"municipio": municipio,
+"area_ha": area_ha
+}
+
+    return produtor
+
+
+print("Modulo de cadastro de produtor rural iniciado")
